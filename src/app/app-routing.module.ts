dcb80675325610c38f3861bec921e740f2b74e5f
AeenPah <139072357+AeenPah@users.noreply.github.com>
2023-08-16 12:43:14 +0330

    primary setting for profile(html,css)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 import { SingUpComponent } from './sing-up/sing-up.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'signup',component:SingUpComponent},
+  {path:'profile',component:ProfileComponent},
 ];
 
 @NgModule({
