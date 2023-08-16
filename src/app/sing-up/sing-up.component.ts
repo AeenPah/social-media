f2625851ac1e8ae86a9f3a78871d08959f2cf893
AeenPah <139072357+AeenPah@users.noreply.github.com>
2023-08-16 09:45:50 +0330

    sign-up connect to db.json
    create connection between sign-up.component and local server.

--- src/app/sing-up/sing-up.component.ts
+++ src/app/sing-up/sing-up.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder } from '@angular/forms'
 
@@ -11,7 +12,7 @@ export class SingUpComponent implements OnInit {
 
   formValue !: FormGroup;
 
-  constructor (private formBuilder:FormBuilder){}
+  constructor (private formBuilder:FormBuilder,private http:HttpClient){}
 
   ngOnInit(): void {
     this.formValue = this.formBuilder.group({
@@ -19,6 +20,13 @@ export class SingUpComponent implements OnInit {
       username : [''],
       email: [''],
       password : ['']
+    })
+  }
+
+  submitSignUp(){
+    this.http.post('http://localhost:3000/users',this.formValue.value).subscribe(res => {
+      console.log("http posted!!");
+      this.formValue.reset();
     })
   }
 }
