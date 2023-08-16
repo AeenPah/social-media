d68ef8fbf66c54aefffc26699b3cafa38b6e8d61
AeenPah <139072357+AeenPah@users.noreply.github.com>
2023-08-16 08:59:21 +0330

    sign-up component
    create sign-up component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
