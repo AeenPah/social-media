ca7f95a66f76dd952c6d40ce6d9987c13aa1a85f
AeenPah <139072357+AeenPah@users.noreply.github.com>
2023-08-16 09:24:13 +0330

    set FormGroup

--- src/app/sing-up/sing-up.component.ts
+++ src/app/sing-up/sing-up.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder } from '@angular/forms'
+
 
 @Component({
   selector: 'app-sing-up',
   templateUrl: './sing-up.component.html',
   styleUrls: ['./sing-up.component.css']
 })
-export class SingUpComponent {
+export class SingUpComponent implements OnInit {
 
+  formValue !: FormGroup;
+
+  constructor (private formBuilder:FormBuilder){}
+
+  ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      fullName : [''],
+      username : [''],
+      email: [''],
+      password : ['']
+    })
+  }
 }
