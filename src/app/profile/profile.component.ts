28439fb1017ed459f69094b3c6d46a6037c936ca
AeenPah <139072357+AeenPah@users.noreply.github.com>
2023-08-17 09:08:21 +0330

    fix bugs

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,24 +12,25 @@ export class ProfileComponent implements OnInit {
   user: any ;
   newuser: any ;
   postDate: Date = new Date;
-  posttx !:string;
-  test = ['another post','another'];
+  posttxt !:string;
+  test : string[] = ['']
 
   constructor(private userDataService:UserDataService,private http:HttpClient){}
 
   ngOnInit(): void {
     this.user = this.userDataService.userData;
-    console.log(this.user.post);
+    // console.log(this.user.post);
   }
 
   postText(){
-    this.test.push(this.posttx);
+    // if (this.user.posts.lenght > 1) {
+      // }
+    this.test = this.user.posts;
+    console.log(this.user.posts.lengh)
+    this.test.push(this.posttxt);
     this.user.posts = this.test;
-    // this.newuser = this.user.push(this.test);
     this.http.put<any>('http://localhost:3000/users/'+this.user.id,this.user).subscribe(res =>{
       console.log(res)
     })
-    console.log(this.newuser);
-
   }
 }
--- src/app/sing-up/sing-up.component.ts
+++ src/app/sing-up/sing-up.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder } from '@angular/forms'
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,9 +12,9 @@ import { FormGroup,FormBuilder } from '@angular/forms'
 export class SingUpComponent implements OnInit {
 
   formSignUP !: FormGroup;
-  
+  primaryPosts = ['',''];
 
-  constructor (private formBuilder:FormBuilder,private http:HttpClient){}
+  constructor (private formBuilder:FormBuilder,private http:HttpClient,private router:Router){}
 
   ngOnInit(): void {
     this.formSignUP = this.formBuilder.group({
@@ -21,14 +22,17 @@ export class SingUpComponent implements OnInit {
       username : [''],
       email: [''],
       password : [''],
-      posts:['','']
+      posts:[''] 
     })
   }
 
   submitSignUp(){
+    this.formSignUP.value.posts = this.primaryPosts;
     this.http.post('http://localhost:3000/users',this.formSignUP.value).subscribe(res => {
       console.log("http posted!!");
       this.formSignUP.reset();
+      this.router.navigate(['/login']);
     })
+
   }
 }
