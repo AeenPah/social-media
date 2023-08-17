b72f869ef489fcb4ef7b40b2a6ee7af2fcafad0f
AeenPah <139072357+AeenPah@users.noreply.github.com>
2023-08-17 10:37:58 +0330

    primary home
    now post is showed in home page
    (primary edition)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  allPostsInf:any;
+  allUsersInf:any;
+
+  constructor(private http:HttpClient){}
+  
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:3000/homePosts').subscribe(res => {
+      this.allPostsInf = res;
+      console.log(this.allPostsInf);
+    })
+    this.http.get<any>('http://localhost:3000/users').subscribe(res => {
+      this.allUsersInf = res;
+      console.log(this.allUsersInf);
+    })
+  }
 }
