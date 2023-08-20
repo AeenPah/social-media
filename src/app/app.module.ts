08cf37ebf5183ea05ce7979ccdf3d61a5f65d8b5
AeenPah <139072357+AeenPah@users.noreply.github.com>
2023-08-20 12:31:32 +0330

    delete sing-up & login comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SingUpComponent } from './sing-up/sing-up.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
@@ -18,8 +16,6 @@ import { AuthModule } from './auth/auth.module';
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    LoginComponent,
-    SingUpComponent,
     ProfileComponent,
     UserProfileComponent,
   ],
