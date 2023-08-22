b929595b8d9bfec59a2f16df6c0c700ceda47c8b
AeenPah <139072357+AeenPah@users.noreply.github.com>
2023-08-22 12:06:59 +0330

    create comp
    dashboard comp in home module.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,6 +7,7 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { FormsModule } from '@angular/forms';
     NavBarComponent,
     ProfileComponent,
     UserProfileComponent,
+    DashboardComponent,
   ],
   imports: [CommonModule, HomeRoutingModule, FormsModule],
 })
